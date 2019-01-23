1aed4a7b5e3bb58a352353ac315939e291dc4250
Leon-kfd <347040447@qq.com>
2019-01-23 23:06:22 +0800

    滚动条样式

--- src/App.vue
+++ src/App.vue
@@ -40,38 +40,6 @@ body {
   font-family: "Helvetica Neue", Helvetica, "PingFang SC", "Hiragino Sans GB",
     "Microsoft YaHei", "微软雅黑", Arial, sans-serif;
 }
-/* 滚动条样式 */
-*::-webkit-scrollbar {
-  width: 12px;
-  height: 12px;
-  background-color: #dddde2;
-}
-*::-webkit-scrollbar-thumb {
-  border-radius: 10px;
-  background-color: #667;
-  background-image: -webkit-linear-gradient(
-    45deg,
-    transparent 10%,
-    rgba(255, 255, 255, 0.3) 10%,
-    rgba(255, 255, 255, 0.3) 20%,
-    transparent 20%,
-    transparent 30%,
-    rgba(255, 255, 255, 0.3) 30%,
-    rgba(255, 255, 255, 0.3) 40%,
-    transparent 40%,
-    transparent 50%,
-    rgba(255, 255, 255, 0.3) 50%,
-    rgba(255, 255, 255, 0.3) 60%,
-    transparent 60%,
-    transparent 70%,
-    rgba(255, 255, 255, 0.3) 70%,
-    rgba(255, 255, 255, 0.3) 80%,
-    transparent 80%,
-    transparent 90%,
-    rgba(255, 255, 255, 0.3) 90%,
-    rgba(255, 255, 255, 0.3)
-  );
-}
 ul,
 li {
   list-style: none;
@@ -90,5 +58,47 @@ li {
   clear: both;
   visibility: hidden;
   height: 0;
+}
+/* 滚动条样式 */
+*::-webkit-scrollbar {
+  width: 8px;
+  height: 8px;
+}
+*::-webkit-scrollbar-thumb {
+  background-color: rgba(0, 0, 0, 0.2);
+  border-radius: 10px;
+  box-shadow: inset 1px 1px 0 rgba(0, 0, 0, 0.1);
+}
+*::-webkit-scrollbar-track {
+  border-radius: 10px;
+  box-shadow: inset 0 0 6px rgba(0, 0, 0, 0);
+}
+/*正常情况下滑块的样式*/
+*::-webkit-scrollbar-thumb {
+  background-color: rgba(0, 0, 0, 0.2);
+  border-radius: 10px;
+  -webkit-box-shadow: inset 1px 1px 0 rgba(0, 0, 0, 0.1);
+}
+/*鼠标悬浮在该类指向的控件上时滑块的样式*/
+*:hover::-webkit-scrollbar-thumb {
+  background-color: rgba(0, 0, 0, 0.4);
+  border-radius: 10px;
+  -webkit-box-shadow: inset 1px 1px 0 rgba(0, 0, 0, 0.1);
+}
+/*鼠标悬浮在滑块上时滑块的样式*/
+*::-webkit-scrollbar-thumb:hover {
+  background-color: rgba(0, 0, 0, 0.6);
+  -webkit-box-shadow: inset 1px 1px 0 rgba(0, 0, 0, 0.1);
+}
+/*正常时候的主干部分*/
+*::-webkit-scrollbar-track {
+  border-radius: 10px;
+  -webkit-box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.1);
+  background-color: white;
+}
+/*鼠标悬浮在滚动条上的主干部分*/
+*::-webkit-scrollbar-track:hover {
+  -webkit-box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.4);
+  background-color: rgba(0, 0, 0, 0.01);
 }
 </style>
