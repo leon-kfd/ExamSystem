e590844fb7bd62d2026dbd36878ce4369df8c577
Leon-kfd <347040447@qq.com>
2019-02-18 22:24:46 +0800

    班级管理

--- src/pages/admin/student-manage/score-manage.vue
+++ src/pages/admin/student-manage/score-manage.vue
@@ -1,3 +1,53 @@
 <template>
-  <div>ScoreManage</div>
+  <div class="page"
+       id="ScoreManage">
+    <div class="score-manage-header clear">
+      <div class="filter-box fl">
+        <el-form ref="filterForm"
+                 :model="filterForm"
+                 label-width="80px">
+          <el-form-item label="班级">
+            <el-select v-model="filterForm.class"
+                       size="small">
+              <el-option v-for="(item,index) in classList"
+                         :key="index"
+                         :value="item.value"
+                         :label="item.label"></el-option>
+            </el-select>
+          </el-form-item>
+        </el-form>
+      </div>
+    </div>
+  </div>
 </template>
+<script>
+export default {
+  name: 'ScoreManage',
+  data () {
+    return {
+      classList: [
+        {
+          label: '15信管',
+          value: 1
+        },
+        {
+          label: '15软件',
+          value: 2,
+        }
+      ],
+      filterForm: {
+        class: ''
+      }
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+.score-manage-header {
+  padding: 10px;
+  .filter-box {
+    width: 300px;
+  }
+}
+</style>
+
